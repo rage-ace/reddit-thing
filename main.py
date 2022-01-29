c2796079cdf7914cbb55170032cc104211eff7a2
rage-ace <tzhyt24@gmail.com>
2022-01-29 18:12:03 +0800

    finished main i guess idk

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import os
 import base64
 from dotenv import load_dotenv
 import time
+import datetime
 
 load_dotenv()
 username = os.getenv("REDDIT_USERNAME")
 password = os.getenv("REDDIT_PASSWORD")
 client_id = os.getenv("REDDIT_PERSONAL_USE_SCRIPT")
 client_secret = os.getenv("REDDIT_SECRET")
-webhook_url = os.getenv("REDDIT_WEBHOOK_URL")
+webhook_url = os.getenv("WEBHOOK_URL")
 
 auth = requests.auth.HTTPBasicAuth(base64.b64decode(client_id), base64.b64decode(client_secret))
 
@@ -30,7 +31,25 @@ TOKEN = res['access_token']
 # add authorization to our headers dictionary
 headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
 
+# in the form of <t3_XXXXXX> or <t1_XXXXXX>
+last_saved = requests.get('https://oauth.reddit.com/user/Rageacew/saved',
+                          headers=headers, params={'limit': 1}).json()['data']['children'][0]['data']['name']
+
 while True:
+    # TODO: finish testing lol
 
+    saved_list = requests.get('https://oauth.reddit.com/user/Rageacew/saved',
+                      headers=headers, params={'before': last_saved}).json()['data']['children']
 
-    time.sleep(30)
+    if saved_list:
+        print('New post detected in saved list!', datetime.datetime.now().time())
+        for post in saved_list:
+            if post['kind'] == 't3': # only posts
+                requests.post(webhook_url, json={
+                    "content": f"https://www.reddit.com{post['data']['permalink']}",
+                })
+        last_saved = saved_list[0]['data']['name']
+    else:
+        print('No new post detected', datetime.datetime.now().time())
+
+    time.sleep(10)
