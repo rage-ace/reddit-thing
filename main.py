99eca44b8a1f92879eeed372468d6e03de98e2e2
rage-ace <tzhyt24@gmail.com>
2022-02-03 13:50:50 +0800

    it shld work now???? idk i hate async and dealibng with global vars
    :angry:

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import base64
 from dotenv import load_dotenv
 import time
 import datetime
+import asyncio
 
 load_dotenv()
 username = os.getenv("REDDIT_USERNAME")
@@ -14,7 +15,7 @@ webhook_url = os.getenv("WEBHOOK_URL")
 
 auth = requests.auth.HTTPBasicAuth(base64.b64decode(client_id), base64.b64decode(client_secret))
 
-headers = {'User-Agent': 'reddit app to get saved list v1.0 (by /u/Rageacew)'}
+temp_headers = {'User-Agent': 'reddit app to get saved list v1.0 (by /u/Rageacew)'}
 
 data = {
     "grant_type": "password",
@@ -23,35 +24,53 @@ data = {
 }
 
 res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers).json()
+                    auth=auth, data=data, headers=temp_headers).json()
 
 # convert response to JSON and pull access_token value
 TOKEN = res['access_token']
 
 # add authorization to our headers dictionary
-headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+headers = {**temp_headers, **{'Authorization': f"bearer {TOKEN}"}}
 
-# in the form of <t3_XXXXXX> or <t1_XXXXXX>
-last_saved = requests.get('https://oauth.reddit.com/user/Rageacew/saved',
-                          headers=headers, params={'limit': 1}).json()['data']['children'][0]['data']['name']
+
 
 # this is horrible code
-while True:
-    # TODO: add error catching maybe
-    # TODO: make it such that every two hours the auth token will refresh
+async def get_posts():
+    # in the form of <t3_XXXXXX> or <t1_XXXXXX>
+    last_saved = requests.get('https://oauth.reddit.com/user/Rageacew/saved',
+                              headers=headers, params={'limit': 1}).json()['data']['children'][0]['data']['name']
+    print(f"Last saved: {last_saved}")
+    
+    while True:
+        # TODO: add error catching maybe
 
-    saved_list = requests.get('https://oauth.reddit.com/user/Rageacew/saved',
-                      headers=headers, params={'before': last_saved}).json()['data']['children']
+        saved_list = requests.get('https://oauth.reddit.com/user/Rageacew/saved',
+                          headers=headers, params={'before': last_saved}).json()['data']['children']
 
-    if saved_list:
-        print('New post detected in saved list!', datetime.datetime.now().time())
-        for post in saved_list:
-            if post['kind'] == 't3': # only posts
-                requests.post(webhook_url, json={
-                    "content": f"https://www.reddit.com{post['data']['permalink']}",
-                })
-        last_saved = saved_list[0]['data']['name']
-    else:
-        print('No new post detected', datetime.datetime.now().time())
+        if saved_list:
+            print('New post detected in saved list!', datetime.datetime.now().strftime('%c'))
+            for post in saved_list:
+                if post['kind'] == 't3': # only posts
+                    requests.post(webhook_url, json={
+                        "content": f"https://www.reddit.com{post['data']['permalink']}",
+                    })
+            last_saved = saved_list[0]['data']['name']
+        else:
+            print('No new post detected', datetime.datetime.now().strftime('%c'))
 
-    time.sleep(60)
+        await asyncio.sleep(60)
+
+async def refresh_tokens():
+    while True:
+        await asyncio.sleep(3570)  # staggered in case weird shit happens
+        print('Refreshing tokens...', datetime.datetime.now().strftime('%c'))
+        res = requests.post('https://www.reddit.com/api/v1/access_token',
+                            auth=auth, data=data, headers=temp_headers).json()
+        TOKEN = res['access_token']
+        headers['Authorization'] = f'bearer {TOKEN}'
+
+
+async def main():
+    await asyncio.gather(get_posts(), refresh_tokens())
+
+asyncio.run(main())
